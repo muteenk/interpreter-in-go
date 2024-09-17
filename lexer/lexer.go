257f36b60f528aa576507f4f820cf9e50b3ab00c
muteenk <kundangarmuteen@gmail.com>
2024-09-17 22:55:33 +0530

    Updating Lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -5,23 +5,73 @@ import (
 )
 
 
-
 type Lexer struct {
 
   input string
-  position int
+  inputSize int
+  readCursor int
+  cursor int
   ch byte
 
 }
 
 
-
-func New (input string) *Lexer{
-  
-  l := &Lexer{input: input}
+// Lexer Constructor
+func newLexer (input string) *Lexer{
+  lex := &Lexer{input: input, inputSize: len(input)}
+  lex.readChar()
   return l
+}
+
+
+// Advance to the next character
+func (lex *Lexer) readChar() {
+  
+  if lex.readCursor >= lex.inputSize {
+    lex.ch = 0;
+  } else {
+    lex.ch = lex.input[lex.readCursor]
+  }
+
+  lex.cursor = lex.readCursor
+  lex.readCursor++; 
 
 }
 
+
+func (lex *Lexer) nextToken() token.Token {
+  var tok token.Token
+  
+  switch lex.ch {
+    
+    case '=':
+      tok = newToken(token.ASSIGN, lex.ch)
+    case ';':
+      tok = newToken(token.SEMICOLON, lex.ch)
+    case '(':
+      tok = newToken(token.LPAREN, lex.ch)
+    case ')':
+      tok = newToken(token.RPAREN, lex.ch)
+    case '{':
+      tok = newToken(token.LBRACE, lex.ch)
+    case '}':
+      tok = newToken(token.RBRACE, lex.ch)
+    case ',':
+      tok = newToken(token.COMMA, lex.ch)
+    case '+':
+      tok = newToken(token.PLUS, lex.ch)
+    case 0:
+      tok.Literal = ""
+      tok.Type = token.EOF
+  }
+
+  lex.readChar()
+  return tok
+}
+
+
+func newToken(tokenType string, ch byte){
+  return token.Token{Type: tokenType, Literal: string(ch)}
+}
 
 
